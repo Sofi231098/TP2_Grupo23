429849515467e682ab166fde5ab50085df271945
Sofia Trinidad Yañez <Sofia Trinidad Yaez@DESKTOP-LC3B0A5>
2023-05-21 19:39:30 -0300

    desarrollo de la capa controller y model de las paginas Index.html y contacto-form.html.Tambien el desarrollo de un nuevo html donde se agregan  y modifican los objetivos de la pagina index.Desarrollo de una lista donde se encuentra un array donde guardamos los objetivos

--- TP2_Grupo23/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -1,15 +1,36 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ar.edu.unju.fi.model.Contactos;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
 @Controller
-class ContactoController {
+@RequestMapping("/")
+	public class ContactoController {
+	
 	
 	@GetMapping("/contacto") 
-	public String contacto(){
+		public String getContacto(Model model){
+	//	Contacto contacto = new Contacto();
+		model.addAttribute( "contacto", new Contactos()); //enviar un pbjeto a la pagina, agrega un atributo a la pagina llamada contacto
 		return "contacto-form";
 	}
-
+	
+	@PostMapping("/contacto")
+	public String postContacto(Model model, Contactos contacto ) {
+		System.out.println(contacto.toString()); //Muestro por consola los atributos de contacto.
+		model.addAttribute( "contacto", new Contactos()); //Se envia un nuevo objeto para que se reinicie el formulario. 
+		model.addAttribute("datos", "Datos enviados Correctamente");
+		return "contacto-form";
+		
+	}
+	
+	
 }
---
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/controller/InicioController.java
@@ -0,0 +1,87 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ar.edu.unju.fi.listas.ListaIndex;
+import ar.edu.unju.fi.model.IndexModel;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/inicio")
+public class InicioController {
+	ListaIndex listaIndex = new ListaIndex();
+	
+	@GetMapping("/lisInicio")
+	public String getIndex(Model model) {
+		model.addAttribute("index", listaIndex.getIndex());
+		return "index";
+	}
+	
+	
+	@GetMapping("/nuevoobj")
+	public String getNuevoObjetivo(Model model) {
+		boolean edicion = false;
+		model.addAttribute("objetivo", new IndexModel());
+		model.addAttribute("edicion" , edicion);
+		return "nuevoo_objetivo";
+	}
+	
+	@PostMapping("/guardar")
+	public ModelAndView getGuardarObjetivo(@ModelAttribute("objetivo") IndexModel indexModel) {
+		ModelAndView modelView = new ModelAndView("index");
+		listaIndex.getIndex().add(indexModel);
+		modelView.addObject("index", listaIndex.getIndex());
+		return modelView;
+		
+	}
+	
+	@GetMapping("/modificar/{objetivos}")
+	public String getModificarObjetivo(Model model, @PathVariable(value="objetivos") int objetivos) {
+		IndexModel objetivoEncontrado = new IndexModel();
+		boolean edicion = true;
+		for(IndexModel obj : listaIndex.getIndex()) {
+			if(obj.getId() == objetivos) {
+				objetivoEncontrado = obj;
+				break;
+			}
+		}
+		System.out.println(objetivoEncontrado.getId());
+		model.addAttribute("objetivo", objetivoEncontrado);
+		model.addAttribute("edicion" , edicion);
+		return "nuevoo_objetivo";
+	}
+	
+	@PostMapping("/modificar")
+	public String modificarObjetivo(@ModelAttribute("objetivo") IndexModel indexModel) { 
+		System.out.println(indexModel.getObjetivos() + indexModel.getId());
+		for(IndexModel obj: listaIndex.getIndex()) {
+			System.out.println(obj.getObjetivos() + obj.getId() );
+			if(obj.getId() == indexModel.getId()) {
+				obj.setObjetivos(indexModel.getObjetivos());
+				//System.out.println(obj.getObjetivos());
+				break;
+			}		
+		}
+		return "redirect:/inicio/lisInicio";
+	} 
+	
+	@GetMapping("/eliminar/{objetivos}")
+	public String eliminarObjetivo(@PathVariable(value="objetivos") int objetivos) {
+		System.out.println(objetivos);
+		for(IndexModel obj: listaIndex.getIndex()) {
+			System.out.println(obj.getId() + obj.getObjetivos());
+			if(obj.getId() == objetivos){
+				listaIndex.getIndex().remove(obj);
+			break;
+				}
+		}
+		
+		return "redirect:/inicio/lisInicio";
+	}
+
+}
