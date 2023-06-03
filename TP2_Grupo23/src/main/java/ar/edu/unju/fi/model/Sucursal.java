aff3e3595fc675849696ba33029ea5f2d25c8a25
Marisol P Castillo <Marisol P Castillo@LAPTOP-GU1KM1HI>
2023-06-03 15:49:45 -0300

    Inyencción de dependecia y validaciones en página sucursales. Añadí la dependecia para la validación de datos en el archivo pom.xml

--- TP2_Grupo23/src/main/java/ar/edu/unju/fi/model/Sucursal.java
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/model/Sucursal.java
@@ -1,20 +1,59 @@
 package ar.edu.unju.fi.model;
 
+import org.springframework.stereotype.Component;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+
+/**
+ * Representa una sucursal del sistema
+ * @author Marisol P Castillo
+ * @version 1.0 date: 01/06/23
+ *
+ */
+
+
+
+@Component
 public class Sucursal {
-	private String codigo;
-	private String nombre;
-	private String provincia;
-	private String direccion;
-	private String telefono;
-	private String horario;
+	
+	@Positive(message="Debe ingresar un número válido.")
+	/**Representa el código de la sucursal*/private String codigo;
+	@NotEmpty(message="El nombre no puede quedar vacío.")
+	/**Representa el nombre de la sucursal*/private String nombre;
+	@NotBlank(message="Debe seleccionar una provincia.")
+	/**Representa la provincia de la sucursal*/private String provincia;
+	@Size(min=5, max=100, message="La dirección debe tener entre 5 y 100 caracteres.")
+	/**Representa la dirección de la sucursal*/private String direccion;
+	@NotEmpty(message="Debe ingresar un número de teléfono.")  //opcional creo yo no todos tienen telefono por ej si es nueva la sucursal.
+	/**Representa el teléfono de la sucursal*/private String telefono;
+	@NotBlank(message="Debe seleccionar un horario de atención.")
+	/**Representa el horario de la sucursal*/private String horario;
 	
 	
+	
+	//--------------Constructores-------------------
+	
+	/**
+	 * Constructor por defecto
+	 */
 	public Sucursal() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	
-	
+	/**
+	 * Constructor Paramétrizado
+	 * @param codigo es el código de la sucursal
+	 * @param nombre es el nombre de la sucursal
+	 * @param provincia es la provincia de la sucursal
+	 * @param direccion es la dirección de la sucursal
+	 * @param telefono es el telefono de la sucursal
+	 * @param horario es el horario de la sucursal
+	 */
 	
 	public Sucursal(String codigo, String nombre, String provincia, String direccion, String telefono, String horario) {
 		super();
@@ -26,7 +65,10 @@ public class Sucursal {
 		this.horario = horario;
 	}
 
-
+/**
+ * Getters y Setters
+ * @return
+ */
 
 	public String getCodigo() {
 		return codigo;
