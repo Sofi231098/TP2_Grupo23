f7a65eb8244f24a1b52380129160e3e9a89285f8
Farja Naima Blanca <Farja Naima Blanca@DESKTOP-RNP7A0T>
2023-06-05 00:05:38 -0300

    Documentacion, arreglos de html, estilos de Bootstrap.

--- TP2_Grupo23/src/main/java/ar/edu/unju/fi/model/Producto.java
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -2,20 +2,50 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+
+
+/**
+ * Representa los articulos del sistema
+ * @author Farja Naima Blanca
+ * @version 1.0 dtae: 03/06/2023
+ */
 @Component
 public class Producto {
-
-	private int codigo;
-    private String nombre;
-    private double precio;
-    private int descuento;
-    private String  categoria;
+	
+	@NotEmpty(message="Debe ingresar el codigo del articulo")
+	/**Representa el codigo del articulo*/private String codigo;
+	@NotEmpty(message="Este campo no puede estar vacio")
+	/**Representa el nombre del articulo*/private String nombre;
+	@PositiveOrZero(message="El valor debe ser un numero positivo")
+	/**Representa el precio del articulo*/private double precio;
+	@PositiveOrZero(message="Descuento desde 0% a 50%")
+	/**Representa el descuento que se va a realizar en el articulo*/private int descuento;
+    @NotBlank(message="Debe elegir una categoria")
+    /**Representa la categoria del articulo*/private String  categoria;
+    
+    //----------Constructores--------------
+    
+    /**
+     * Constructores por defecto
+     */
     
     public Producto() {
 		// TODO Auto-generated constructor stub
 	}
     
-	public Producto(int codigo, String nombre, double precio, int descuento, String categoria) {
+    /**
+     * Constructor parametrisado
+     * @param codigo es el codigo del articulo
+     * @param nombre es el nombre del articulo
+     * @param precio es el precio del articulo
+     * @param descuento es el descuento que se le va a realizar al articulo
+     * @param categoria es la categoria del articulo
+     */
+    
+	public Producto(String codigo, String nombre, double precio, int descuento, String categoria) {
 		super();
 		this.codigo = codigo;
 		this.nombre = nombre;
@@ -25,12 +55,19 @@ public class Producto {
 		this.categoria = categoria;
 	}
 	
-	public int getCodigo() {
+	/**
+	 * Getters y Setters
+	 * @return
+	 */
+
+	public String getCodigo() {
 		return codigo;
 	}
-	public void setCodigo(int codigo) {
+
+	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
