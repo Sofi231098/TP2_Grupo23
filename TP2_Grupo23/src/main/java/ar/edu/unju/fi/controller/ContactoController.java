ca51ce6d1d8e1cc7809346df966ca486621fabe3
Farja Naima Blanca <Farja Naima Blanca@DESKTOP-RNP7A0T>
2023-06-10 17:59:21 -0300

    Se arreglo un bug relacionado con las validaciones de la pagina productos. Correccion de la capa model y lista. Se edito el archivo CSS para lograr una mejor armonia entre las paginas. Correccion de la capa controller de la pagina contacto para agregar un mensaje al enviar correctamente el formulario.

--- TP2_Grupo23/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 import ar.edu.unju.fi.model.Contactos;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -38,18 +41,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 	 * @return  el retorno es la pagina contacto-form.html, vista
 	 */
 	@PostMapping("/contacto")
-	public String postContacto(Model model ,@Valid Contactos contacto, BindingResult  result ) {
-		if(result.hasErrors()) {
-			model.addAttribute("contacto-form");
-			model.addAttribute( "contacto", contacto);
-			return "contacto-form";
-		}
-			model.addAttribute( "contacto", contacto); //Se envia un nuevo objeto para que se reinicie el formulario. 
-			model.addAttribute("datos", "Datos enviados Correctamente");
-			System.out.println(contacto.toString()); //Muestro por consola los atributos de contacto.
-			return "contacto-form";
-			
+    public ModelAndView postContacto(Model model ,@Valid @ModelAttribute(value = "contacto") Contactos contacto, BindingResult  result ) {
+        ModelAndView modelAndView = new ModelAndView("contacto-form");
+        if(result.hasErrors()) {
+            System.out.println(contacto.toString());
+            modelAndView.setViewName("contacto-form");
+            model.addAttribute("contacto-form");
+            modelAndView.addObject("contacto", contacto);
+            return modelAndView;
+        }
+        	
+            model.addAttribute( "contacto", contacto); //Se envia un nuevo objeto para que se reinicie el formulario. 
+            model.addAttribute("datos", "Datos enviados Correctamente");
+            System.out.println(contacto.toString()); //Muestro por consola los atributos de contacto.
+            return modelAndView;
+          
+          
+	
 	}
-	
-	
 }
--- TP2_Grupo23/src/main/java/ar/edu/unju/fi/model/Producto.java
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Component;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.NotNull;
+
 
 
 /**
  * Representa los articulos del sistema
  * @author Farja Naima Blanca
- * @version 1.0 dtae: 03/06/2023
+ * @version 1.0 date: 03/06/2023
  */
 @Component
 public class Producto {
@@ -19,10 +20,12 @@ public class Producto {
 	/**Representa el codigo del articulo*/private String codigo;
 	@NotEmpty(message="Este campo no puede estar vacio")
 	/**Representa el nombre del articulo*/private String nombre;
-	@PositiveOrZero(message="El valor debe ser un numero positivo")
-	/**Representa el precio del articulo*/private double precio;
-	@PositiveOrZero(message="Descuento desde 0% a 50%")
-	/**Representa el descuento que se va a realizar en el articulo*/private int descuento;
+	@NotEmpty(message="Debe agregar una descripcion breve del producto")
+	/**Representa la descripcion del articulo*/ private String descripcion;
+	@NotNull(message="El valor debe ser un numero positivo")
+	/**Representa el precio del articulo*/private Double precio;
+	@NotNull(message="El descuento debe ser de entre 0% y 50%")
+	/**Representa el descuento que se va a realizar en el articulo*/private Integer descuento;
     @NotBlank(message="Debe elegir una categoria")
     /**Representa la categoria del articulo*/private String  categoria;
     
@@ -43,18 +46,24 @@ public class Producto {
      * @param precio es el precio del articulo
      * @param descuento es el descuento que se le va a realizar al articulo
      * @param categoria es la categoria del articulo
-     */
-    
-	public Producto(String codigo, String nombre, double precio, int descuento, String categoria) {
+     */	
+	
+	public Producto(@NotEmpty(message = "Debe ingresar el codigo del articulo") String codigo,
+			@NotEmpty(message = "Este campo no puede estar vacio") String nombre,
+			@NotEmpty(message = "Debe agregar una descripcion breve del producto") String descripcion,
+			@NotNull(message = "El valor debe ser un numero positivo") Double precio,
+			@NotNull(message = "El descuento debe ser de entre 0% y 50%") Integer descuento,
+			@NotBlank(message = "Debe elegir una categoria") String categoria) {
 		super();
 		this.codigo = codigo;
 		this.nombre = nombre;
+		this.descripcion = descripcion;
 		this.precio = precio;
 		this.descuento = descuento;
 		getPreciodescuento();
 		this.categoria = categoria;
 	}
-	
+
 	/**
 	 * Getters y Setters
 	 * @return
@@ -74,18 +83,31 @@ public class Producto {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public double getPrecio() {
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecio() {
 		return precio;
 	}
-	public void setPrecio(double precio) {
+
+	public void setPrecio(Double precio) {
 		this.precio = precio;
 	}
-	public int getDescuento() {
+
+	public Integer getDescuento() {
 		return descuento;
 	}
-	public void setDescuento(int descuento) {
+
+	public void setDescuento(Integer descuento) {
 		this.descuento = descuento;
 	}
+
 	public double getPreciodescuento() {
 		if (descuento >= 0 || descuento <= 50) {
 			
