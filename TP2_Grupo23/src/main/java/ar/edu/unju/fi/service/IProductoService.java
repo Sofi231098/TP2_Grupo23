03af3e6a00f99256bce3ca206e60335af692e2ec
Farja Naima Blanca <Farja Naima Blanca@DESKTOP-RNP7A0T>
2023-06-20 18:11:45 -0300

    Creacion de la rama "Remplazar_Codigo_Productos"
    En esta rama se realizo la tarea de remplazar el codigo de los articulos
    por un ID de tipo long, para implementarlo en BD.

--- TP2_Grupo23/src/main/java/ar/edu/unju/fi/service/IProductoService.java
+++ TP2_Grupo23/src/main/java/ar/edu/unju/fi/service/IProductoService.java
@@ -30,7 +30,7 @@ public interface IProductoService {
 	 * @param codigo el código del producto a buscar
 	 * @return el objeto Producto encontrado o null si no se encuentra ningún producto con el código especificado
 	 */
-	public Producto getBy(String codigo);
+	public Producto getBy(Long id);
 	
 	/**
 	 * Modifica un producto existente.
